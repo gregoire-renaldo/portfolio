7c64b1b45ff07fe1d3a7e501f18ddfa3f392a891
gregoire renaldo <gregoire.renaldo@gmail.com>
2022-01-06 14:25:40 +0100

    add fontawesome

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import userData from "@constants/data";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
 
 
 
@@ -10,12 +13,11 @@ export default function Footer() {
         <div className="h-0.5 w-full bg-white dark:bg-gray-700"></div>
         <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
           <div>
-            <p> Grégoire Renaldo.</p>
+            <p> Created with React/Next.js </p>
           </div>
           <div>
-            {/* Support me by keeping this in the footer, please. :) */}
             <p>
-              Made with love by
+              Made with love <FontAwesomeIcon icon={faHeart} spin color={"red"}  />    by
               <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
                 <a
                   className="hover:bg-red-500 rounded-md px-2 py-1  hover:text-gray-50"
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
+// font awesome
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
